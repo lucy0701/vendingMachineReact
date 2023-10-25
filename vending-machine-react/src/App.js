40ffc7335bb1596a1acabdbeafda9394510590b3
Crystal P <sj8871@gmail.com>
2023-10-26 05:13:24 +0900

    커스텀
    기본커스텀

--- vending-machine-react/src/App.js
+++ vending-machine-react/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 className="title">자판기</h2>
       </header>
+      
+      <div className="App-body">
+        <div className="vending-body">
+            <div className="body-left">
+              <div className="item-display"></div>
+              <div className="machine-coin"></div>
+            </div>
+
+            <div className="body-rigth">
+              <h2>CRYSTAL</h2>
+              <div className="total-screen">
+                <span className = "total-num">123</span>
+              </div>
+              <button className="ent-coin-btn"></button>
+              <button className="return-coin-btn"></button>
+              <div className="get-item-box"></div>
+            </div>
+
+        </div>
+      </div>
+
+      <footer className="App-footer">
+        <div className="footer-box">
+          <button className="footer-btn">아이템</button>
+          <button className="footer-btn">관리자</button>
+        </div>
+      </footer>
     </div>
   );
 }
